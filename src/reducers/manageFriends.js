9fa7179461f772b47278d5eadfa2aa6877c7c5a6
cpan001 <github email address>
2017-11-15 15:25:56 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      const newFriends = [...state.friends, action.friend];
+      return { friends: newFriends };
+      break;
+    case "REMOVE_FRIEND":
+      const newFriends2 = state.friends.filter(
+        friend => friend.id !== action.id
+      );
+      return { friends: newFriends2 };
+      break;
+    default:
+      return state;
+  }
 }
